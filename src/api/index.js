c38be04cf62b3c10474d1533408c0f005b41596e
Andrew Zey <andrew@andrewzey.com>
2016-08-11 07:41:45 -0700

    Add images endpoints

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
 import { version } from '../../package.json';
 import { Router } from 'express';
-import facets from './facets';
+import images from './images';
 
 export default ({ config, db }) => {
 	let api = Router();
 
-	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
+	// mount the images resource
+	api.use('/images', images({ config, db }));
 
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
